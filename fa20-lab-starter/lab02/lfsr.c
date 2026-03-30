2b74c697d027cc2d3e325263b6031f789be2024c
Tux Node <tuxnode@outlook.com>
2026-03-30 09:59:39 +0800

    Finish Lab02 lfsr

--- fa20-lab-starter/lab02/lfsr.c
+++ fa20-lab-starter/lab02/lfsr.c
@@ -6,5 +6,16 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+  uint16_t bit0, bit2, bit3, bit5;
+
+  bit0 = *reg & 1;
+  bit2 = (*reg >> 2) & 1;
+  bit3 = (*reg >> 3) & 1;
+  bit5 = (*reg >> 5) & 1;
+
+  uint16_t feedback = bit0 ^ bit2 ^ bit3 ^ bit5;
+
+  *reg = *reg >> 1;
+  *reg = *reg | (feedback << 15);
 }
 
