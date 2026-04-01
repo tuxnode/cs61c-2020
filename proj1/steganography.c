95c40038411fdf15b704a12dca6b20d7cc0a1d4d
Tux Node <tuxnode@outlook.com>
2026-04-01 09:04:54 +0800

    Finish porj1 staganography

--- proj1/steganography.c
+++ proj1/steganography.c
@@ -22,12 +22,59 @@
 Color *evaluateOnePixel(Image *image, int row, int col)
 {
 	//YOUR CODE HERE
+	Color *newColor = malloc(sizeof(Color));
+  if (newColor == NULL) return NULL;
+
+  uint8_t b_channal = image->image[row][col].B;
+
+  int lsb = b_channal & 1;
+
+  if (lsb == 1) {
+    newColor->R = newColor->G = newColor->B = 255;
+  } else {
+    newColor->R = newColor->G = newColor->B = 0;
+  }
+
+  return newColor;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
 {
 	//YOUR CODE HERE
+	Image *newImg = malloc(sizeof(Image));
+	if (newImg == NULL) return NULL;
+
+  newImg->rows = image->rows;
+  newImg->cols = image->cols;
+
+  newImg->image = malloc(sizeof(Color *) * newImg->rows);
+  if (newImg->image == NULL) {
+    free(newImg);
+    return NULL;
+  }
+
+  for (int i = 0; i < newImg->rows; i++) {
+    newImg->image[i] = malloc(sizeof(Color) * newImg->cols);
+    if (newImg->image[i] == NULL) {
+      free(newImg);
+      free(newImg->image);
+      return NULL;
+    }
+    for (int j = 0; j < newImg->cols; j++) {
+      Color *tempColor = evaluateOnePixel(image, i, j);
+      if (tempColor == NULL) {
+        free(newImg->image[i]);
+        free(newImg->image);
+        free(newImg);
+        return NULL;
+      }
+      newImg->image[i][j] = *tempColor;
+      free(tempColor);
+    }
+  }
+
+  return newImg;
 }
 
 /*
@@ -45,5 +92,26 @@ Make sure to free all memory before returning!
 */
 int main(int argc, char **argv)
 {
-	//YOUR CODE HERE
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+    return -1;
+  }
+
+  Image *originalImage = readData(argv[1]);
+  if (originalImage == NULL) {
+    return -1;
+  }
+
+  Image *secretImage = steganography(originalImage);
+  if (secretImage == NULL) {
+    freeImage(originalImage);
+    return -1;
+  }
+
+  writeData(secretImage);
+
+  freeImage(originalImage);
+  freeImage(secretImage);
+
+  return 0;
 }
